ed19d11622a03bb10db5a013c4804ce0d92096e9
Shneddly <suomysaila@gmail.com>
2024-04-24 21:47:28 -0700

    Added README.md Generate basic app structure

---
+++ README.md
@@ -0,0 +1,4 @@
+CSS 545 - Mobile Computing Group Project
+David, Ned, Rob
+nyooom! Restaurant/Bar suggestion app
+Prototype Repository
---
+++ app/src/main/java/com/example/css545_prototype/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.css545_prototype
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.css545_prototype.ui.theme.CSS545_PrototypeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CSS545_PrototypeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CSS545_PrototypeTheme {
+        Greeting("Android")
+    }
+}
