284e188d31c486f6c31428c5be08dd30ec585690
Shneddly <suomysaila@gmail.com>
2024-04-26 21:22:40 -0700

    added comment

--- app/src/main/java/com/example/css545_prototype/MainActivity.kt
+++ app/src/main/java/com/example/css545_prototype/MainActivity.kt
@@ -25,7 +25,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
+// change
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
